2b758af457ea92351b8125750d361c3f3150cecb
Emma Pham <emmauyentrinh@gmail.com>
2024-03-19 19:04:19 -0500

    added changes to Subscriptions with button

--- coogify-app/src/components/interface/SubscriptionMain.tsx
+++ coogify-app/src/components/interface/SubscriptionMain.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 export const SubscriptionMain = () => {
   return (
     <div
@@ -8,32 +11,46 @@ export const SubscriptionMain = () => {
         <div className="text-center text-4xl font-bold mb-10 mt-[45px] text-[50px]">
           Subscription
         </div>
-        <div className="w-full rounded-xl md:h-[calc(100vh-140px)] h-auto flex flex-row items-center gap-5 px-5 md:py-5 pb-20 pt-5 relative">
-          <div className="absolute top-0 left-52 text-2xl flex flex-col gap-4 items-center">
+        <div className="w-full rounded-xl md:h-[calc(100vh-140px)] h-auto flex flex-row items-center gap-5 px-5 md:py-5 pb-20 pt-5 relative ">
+          <div className="absolute top-0 left-52 text-2xl flex flex-col gap-4 items-center ">
+          <Link to="">
             Subscription Plans
-            <div className="bg-[#8C8C8C] w-96 h-30 py-9 rounded-lg text-4xl pl-7 font-bold">
+            <div className="w-96 h-30 py-9 rounded-lg text-4xl pl-7 font-bold mb-4 hover:bg-[#ddc5fc] bg-[#d1c9db] shadow-md shadow-[#d0c2e2] cursor-pointer">
               Free
               <div className="text-lg font-normal">$0.00/per month</div>
             </div>
-            <div className="text-[#A263F2] bg-[#8C8C8C] w-96 h-30 py-9 rounded-lg text-4xl pl-7 font-bold">
+            </Link>
+            <div className="text-[#A263F2] w-96 h-30 py-9 rounded-lg text-4xl pl-7 font-bold mb-4 hover:bg-[#ddc5fc] bg-[#d1c9db] shadow-md shadow-[#d0c2e2] cursor-pointer" >
+            <Link to="">
               Premium
               <div className=" text-white text-lg font-normal">
                 $10.99/per month
               </div>
+              </Link>
             </div>
-            <div className="text-[#DCCE51] bg-[#8C8C8C] w-96 h-30 py-9 rounded-lg text-4xl pl-7 font-bold">
+            <div className="text-[#FFFF00] w-96 h-30 py-9 rounded-lg text-4xl pl-7 font-bold hover:bg-[#ddc5fc] bg-[#d1c9db] shadow-md shadow-[#d0c2e2] cursor-pointer">
+            <Link to="">
               Student
               <div className=" text-white text-lg font-normal">
                 $5.99/per month
               </div>
+              </Link>
             </div>
+            
           </div>
           <div className="absolute top-0 right-52 text-2xl flex flex-col gap-4 items-center">
             Current Plan
-            <div className="bg-[#8C8C8C] w-96 h-96 px-9 py-9 rounded-lg pl-7 font-normal">
+            <div className="bg-[#8C8C8C] w-96 h-96 px-9 py-9 rounded-lg pl-7 font-normal   ">
+            <div>
               <div className="text-center">Start Date:</div>
               <div className="text-center">End Date:</div>
               <div className="text-center">Renewal Date:</div>
+              </div>
+              <div className="text-center">
+              <button className="bg-[#BA85FE] hover:bg-[#472670] text-white font-bold py-2 px-4 border border-blue-700 rounded ">
+                      Cancel Trial
+              </button>
+              </div>
             </div>
           </div>
         </div>
