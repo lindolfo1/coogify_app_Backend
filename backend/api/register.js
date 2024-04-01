3b255a0f65d6a32abbb71a1d71bca3b07eeee4e1
lindolfo1 <cuentas@feptec.com>
2024-04-01 15:14:53 -0500

    modified:   backend/api/register.js

--- backend/api/register.js
+++ backend/api/register.js
@@ -9,8 +9,8 @@ import authenticateMiddleware from "../middlewares/authenticate.js";
 
 export default async function handler(req, res) {
   jsonParserMiddleware(req, res);
-  hashPasswordMiddleware(req, res, next);
-  authenticateMiddleware(req, res, next);
+  hashPasswordMiddleware(req, res);
+  authenticateMiddleware(req, res);
   console.log("registering");
   const { firstName, lastName, email, password } = req.body;
   const hashedInput = await hashPassword(password);
